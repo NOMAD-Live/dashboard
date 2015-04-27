dbca4b9f9d6843ae828c29e8d95d7b9343d98990
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-04-27 18:15:37 -0400

    Fix: Splits Past and Present streams.

--- public_html/src/app.js
+++ public_html/src/app.js
@@ -1,45 +1,17 @@
-var stream_info = {
-        'user_display_name': 'bej48snvvthy',
-        'private': false,
-        'kickflip_url': 'https://kickflip.io/e83a515e-fe69-4b19-afba-20f30d56b719',
-        'extra_info': '',
-        'city': 'Montreal',
-        'title': '04/14/2015 12:45 PM',
-        'state': 'Quebec',
-        'type': 'HLS',
-        'user_avatar': 'https://kick-us-east-1.s3.amazonaws.com/nomad-alpha/bej48snvvthy/avatars/avatar.jpg',
-        'description': '',
-        'deleted': false,
-        'user_username': 'bej48snvvthy',
-        'start_lat': '',
-        'time_finished': '04/14/2015 16:46:43',
-        'end_lon': -73.56478346,
-        'time_started': '04/14/2015 16:45:47',
-        'success': true,
-        'start_lon': '',
-        'country': 'Canada',
-        'chat_url': 'Not implemented yet',
-        'stream_id': 'e83a515e-fe69-4b19-afba-20f30d56b719',
-        'length': 55,
-        'thumbnail_url': 'https://kick-us-west-2.s3.amazonaws.com/nomad-alpha/bej48snvvthy/e83a515e-fe69-4b19-afba-20f30d56b719/thumb.jpg',
-        'flags': 0,
-        'end_lat': 45.51771212,
-        'stream_url': 'https://d3sa2dsf6xa6d8.cloudfront.net/nomad-alpha/bej48snvvthy/e83a515e-fe69-4b19-afba-20f30d56b719/vod.m3u8'
-    };
+'use strict';
 
-var stream_info_2 = {'user_display_name': 'gy1x8t48dcst', 'private': false, 'kickflip_url': 'https://kickflip.io/dff1af65-5797-4f7c-90d7-dd58a38f0aa1', 'extra_info': '', 'city': 'Montreal', 'title': '04/22/2015 04:50 PM', 'state': 'Quebec', 'type': 'HLS', 'user_avatar': 'https://kick-us-east-1.s3.amazonaws.com/nomad-alpha/gy1x8t48dcst/avatars/avatar.jpg', 'description': '', 'deleted': false, 'user_username': 'gy1x8t48dcst', 'start_lat': '', 'time_finished': '04/22/2015 20:51:23', 'end_lon': -73.6070529, 'time_started': '04/22/2015 20:50:31', 'success': true, 'start_lon': '', 'country': 'Canada', 'chat_url': 'Not implemented yet', 'stream_id': 'dff1af65-5797-4f7c-90d7-dd58a38f0aa1', 'length': 51, 'thumbnail_url': 'https://kick-us-west-2.s3.amazonaws.com/nomad-alpha/gy1x8t48dcst/dff1af65-5797-4f7c-90d7-dd58a38f0aa1/thumb.jpg', 'flags': 0, 'end_lat': 45.5274959, 'stream_url': 'https://d3sa2dsf6xa6d8.cloudfront.net/nomad-alpha/gy1x8t48dcst/dff1af65-5797-4f7c-90d7-dd58a38f0aa1/vod.m3u8'};
+function isLive(stream) {
+    return stream.length === 0;
+}
 
-var short_info = {stream_url: 'https://d3sa2dsf6xa6d8.cloudfront.net/nomad-alpha/bej48snvvthy/e83a515e-fe69-4b19-afba-20f30d56b719/vod.m3u8'};
-
-var stream_list = [];
-
-stream_list.push(stream_info);
-stream_list.push(stream_info_2);
+function isVOD(stream) {
+  return !isLive(stream);
+}
 
 var ListItemStreamWrapper = React.createClass({
   render: function() {
     return (
-      <div class="streamItem">
+      <div className="col-sm-4 streamItem">
         <OSMFStream {...this.props} />
       </div>
     );
@@ -69,7 +41,7 @@ var OSMFStream = React.createClass({
 
       var parameters = {
           src: stream_url,
-          autoPlay: true,
+          autoPlay: stream_info['length'] === 0,
           verbose: true,
           controlBarAutoHide: true,
           controlBarPosition: "bottom",
@@ -112,17 +84,31 @@ var OSMFStream = React.createClass({
 
 var StreamList = React.createClass({
   render: function() {
+
+    stream_list = this.props.stream_list
+    live_list = stream_list.filter(isLive)
+    vod_list = stream_list.filter(isVOD)
+
     return (
-      <div class="streamList">
-        {this.props.stream_list.map(function(e) {
-           return <ListItemStreamWrapper key={e.stream_id} stream_info={e}/>;
-        })}
+      <div className="streamLists">
+        <h1>Present</h1>
+        <div className="row liveStreamList">
+          {live_list.map(function(e) {
+             return <ListItemStreamWrapper key={e.stream_id} stream_info={e}/>;
+          })}
+        </div>
+        <h1>Past</h1>
+        <div className="row vodStreamList">
+          {vod_list.map(function(e) {
+             return <ListItemStreamWrapper key={e.stream_id} stream_info={e}/>;
+          })}
+        </div>
       </div>
     );
   }
 });
 
-$(".button.getStreams").click(function(){
+$(".getStreams").click(function(){
 
   KICKFLIP_API_URL = "./api"
 
