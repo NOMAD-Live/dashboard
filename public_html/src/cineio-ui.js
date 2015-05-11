1f1a63abdc1d3dfb66df1816fac63e37b448969d
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-05-11 18:40:32 -0400

    Add: Can now display a list.

--- public_html/src/cineio-ui.js
+++ public_html/src/cineio-ui.js
@@ -29,10 +29,57 @@ var CineIOStream = React.createClass({
 
 
 var MainStream = React.createClass({
-    render: function () {
+  render: function () {
     return (
       <div className="cineio-stream center">
         <CineIOStream {...this.props} />
+      </div>
+    )
+  }
+});
+
+
+var CineIOAPIStream = React.createClass({
+  render: function () {
+    var stream = this.props.stream;
+    return (
+      <div className="cineio-stream">
+        <CineIOStream {...this.props} streamId={stream.id} />
+      </div>
+    )
+  }
+});
+
+
+var Project = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var projectSecretKey = React.findDOMNode(this.refs.projectSecretKey).value.trim();
+    
+    // Do nothing if no value
+    if (!projectSecretKey) return;
+    
+    if (this.props.onProjectSecretKeyUpdate) {
+      this.props.onProjectSecretKeyUpdate({secret_key: projectSecretKey});
+    }
+    alert("aaa");
+    return;
+  },
+  getInitialState: function() {
+    return {secret_key:[]};
+  },
+  render: function () {
+    return (
+      <div className="project">
+        <div className="project settings">
+          <input type="text" defaultValue="960dc609fab24e922c3605d1152f48e4" placeholder="Project Secret Key" ref="projectSecretKey" />
+          <button onclick={this.handleSubmit} >Update</button>
+        </div>
+        <div className="stream-list">
+          {mock.streams.map(function(e) {
+             return <CineIOAPIStream key={e.id} stream={e}/>;
+          })}
+        </div>
       </div>
     )
   }
